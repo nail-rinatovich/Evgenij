967fce7e23fdb8b9794adaf0b36521b2ff4b7bda
nail-rinatovich <nailfax1991@yandex.ru>
2024-05-19 10:56:01 +0200

    first commit

---
+++ blog_project/blog_project/urls.py
@@ -0,0 +1,18 @@
+# blog_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+ 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), # новое добавление    
+    path('', include('blog.urls')),
+]
+
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
